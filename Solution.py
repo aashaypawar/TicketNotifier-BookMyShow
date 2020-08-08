9e26aa469f2e1920427e37523ae29f616a4be347
Aashay Pawar <41710824+aashaypawar@users.noreply.github.com>
2020-08-08 22:44:46 +0530

    Create Solution.py

---
+++ Solution.py
@@ -0,0 +1,53 @@
+import argparse
+from bs4 import BeautifulSoup as bs
+import requests
+import smtplib
+import time
+
+bot_mail = ''
+password = ''
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-m", nargs="?", help="Name of movie to track", type=str)
+
+parser.add_argument("-l", nargs="?", help="Location where movie is tracked",
+                    type=str)
+
+parser.add_argument("email", help="Email address to notify if tickets are \
+                    available", type=str)
+args = parser.parse_args()
+
+if args.m and args.l and args.email and bot_mail and password:
+    pass
+else:
+    print("Enter all the arguments\n")
+    print(parser.print_help())
+    quit()
+
+
+def search(mov, loc):
+    req = requests.get('https://www.google.co.in/search?q=bms '+loc+' '+mov)
+    soup = bs(req.text, 'html.parser')
+    tags = soup.findAll('h3')
+    for i in range(3):
+        analyse(tags[i].a.attrs['href'], mov, loc)
+
+
+def mail(mov, loc):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(bot_mail, password)
+    SUBJECT = "bms_alert " + mov + " " + loc
+    TEXT = "Hi Human, \n\nTickets for {} are for sale in {}. Cheers!\n\nYours, \nLab PC".format(mov, loc)
+    msg = 'Subject: {}\n\n{}'.format(SUBJECT, TEXT)
+    server.sendmail(bot_mail, args.email, msg)
+    server.quit()
+
+
+def analyse(link, mov, loc):
+    if 'buytickets' in link:
+        mail(mov, loc)
+        quit()
+
+
+link = search(args.m, args.l)
